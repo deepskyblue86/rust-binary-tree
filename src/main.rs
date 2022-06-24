bebc1d4716e3aeb2c1bf24f18809a48ce04919f5
Angelo Puglisi <angelopuglisi86@gmail.com>
2022-06-24 12:54:43 +0200

    feat: implement last movement based visit
    Restructured the whole code to better deal with "pointers".
    Implemented an iterator visiting the tree by saving the last movement.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::cell::RefCell;
 use std::ops::Deref;
 use std::rc::Rc;
 
+#[derive(Clone)]
 pub struct TreeNodePtr {
     pub ptr: Rc<RefCell<TreeNode>>,
 }
@@ -13,7 +14,11 @@ impl Deref for TreeNodePtr {
     }
 }
 
-pub type TreeNodeLink = Option<TreeNodePtr>;
+#[derive(Clone)]
+pub enum TreeNodeLink {
+    Nil,
+    Ptr(TreeNodePtr),
+}
 
 pub struct TreeNode {
     pub value: u32,
@@ -26,14 +31,26 @@ impl TreeNode {
     pub fn new(value: u32) -> TreeNode {
         TreeNode {
             value,
-            parent: None,
-            left: None,
-            right: None,
+            parent: TreeNodeLink::Nil,
+            left: TreeNodeLink::Nil,
+            right: TreeNodeLink::Nil,
         }
     }
 
+    pub fn has_left(&self) -> bool {
+        match &self.left {
+            TreeNodeLink::Nil => false,
+            TreeNodeLink::Ptr(_) => true,
+        }
+    }
+    pub fn has_right(&self) -> bool {
+        match &self.right {
+            TreeNodeLink::Nil => false,
+            TreeNodeLink::Ptr(_) => true,
+        }
+    }
     pub fn is_leaf(&self) -> bool {
-        self.left.is_none() && self.right.is_none()
+        !self.has_left() && !self.has_right()
     }
 }
 
@@ -53,7 +70,7 @@ impl TreeNodePtr {
         // create the node
         let node_ptr = TreeNodePtr::new(value);
         // set `self` as its parent
-        node_ptr.borrow_mut().parent = Some(self.clone());
+        node_ptr.borrow_mut().parent = TreeNodeLink::Ptr(self.clone());
 
         return node_ptr;
     }
@@ -61,7 +78,7 @@ impl TreeNodePtr {
     pub fn add_left(&mut self, value: u32) -> TreeNodePtr {
         let node_ptr = self.create_child(value);
         // set it as left child of `self`
-        self.borrow_mut().left = Some(node_ptr.clone());
+        self.borrow_mut().left = TreeNodeLink::Ptr(node_ptr.clone());
 
         return node_ptr;
     }
@@ -69,9 +86,34 @@ impl TreeNodePtr {
     pub fn add_right(&mut self, value: u32) -> TreeNodePtr {
         let node_ptr = self.create_child(value);
         // set it as right child of `self`
-        self.borrow_mut().right = Some(node_ptr.clone());
+        self.borrow_mut().right = TreeNodeLink::Ptr(node_ptr.clone());
 
         return node_ptr;
+    }
+
+    pub fn get_left(&self) -> TreeNodeLink {
+        match &self.borrow_mut().left {
+            TreeNodeLink::Nil => TreeNodeLink::Nil,
+            TreeNodeLink::Ptr(left) => TreeNodeLink::Ptr(left.clone()),
+        }
+    }
+    pub fn get_right(&self) -> TreeNodeLink {
+        match &self.borrow_mut().right {
+            TreeNodeLink::Nil => TreeNodeLink::Nil,
+            TreeNodeLink::Ptr(right) => TreeNodeLink::Ptr(right.clone()),
+        }
+    }
+    pub fn get_parent(&self) -> TreeNodeLink {
+        match &self.borrow_mut().parent {
+            TreeNodeLink::Nil => TreeNodeLink::Nil,
+            TreeNodeLink::Ptr(parent) => TreeNodeLink::Ptr(parent.clone()),
+        }
+    }
+}
+
+impl std::fmt::Display for TreeNodePtr {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.borrow_mut().value)
     }
 }
 
@@ -94,13 +136,13 @@ impl Tree {
     }
 
     fn visit_node(node: &TreeNodePtr) {
-        if let Some(left) = &node.borrow_mut().left {
+        if let TreeNodeLink::Ptr(left) = &node.borrow_mut().left {
             Tree::visit_node(&left);
         }
 
         print!("{}, ", node.borrow_mut().value);
 
-        if let Some(right) = &node.borrow_mut().right {
+        if let TreeNodeLink::Ptr(right) = &node.borrow_mut().right {
             Tree::visit_node(&right);
         }
     }
@@ -108,6 +150,150 @@ impl Tree {
     pub fn visit(&self) {
         Tree::visit_node(&self.root);
         println!("");
+    }
+
+    pub fn iter(&self) -> TreeIter {
+        TreeIter::new(&self)
+    }
+}
+
+#[derive(Clone, Copy)]
+enum Movement {
+    UNKNOWN,
+    LEFT,
+    RIGHT,
+    UP,
+}
+
+impl Default for Movement {
+    fn default() -> Self {
+        Movement::UNKNOWN
+    }
+}
+
+#[derive(Clone)]
+struct TreeIter {
+    curr: TreeNodeLink,
+    last_move: Movement,
+    ascended: bool,
+}
+
+impl Default for TreeIter {
+    fn default() -> Self {
+        Self {
+            curr: TreeNodeLink::Nil,
+            last_move: Movement::UNKNOWN,
+            ascended: false,
+        }
+    }
+}
+
+impl TreeIter {
+    pub fn new(t: &Tree) -> TreeIter {
+        let mut it = TreeIter::default();
+        it.curr = TreeNodeLink::Ptr(t.root.clone());
+
+        it.descend();
+
+        return it;
+    }
+
+    fn go_left(&mut self) {
+        if let TreeNodeLink::Ptr(curr) = &self.curr {
+            if let TreeNodeLink::Ptr(left) = curr.get_left() {
+                self.curr = TreeNodeLink::Ptr(left.clone());
+                self.last_move = Movement::LEFT;
+            }
+        }
+    }
+    fn go_right(&mut self) {
+        if let TreeNodeLink::Ptr(curr) = &self.curr {
+            if let TreeNodeLink::Ptr(right) = curr.get_right() {
+                self.curr = TreeNodeLink::Ptr(right.clone());
+                self.last_move = Movement::RIGHT;
+            }
+        }
+    }
+    fn go_up(&mut self) {
+        if let TreeNodeLink::Ptr(curr) = &self.curr {
+            if let TreeNodeLink::Ptr(parent) = curr.get_parent() {
+                self.curr = TreeNodeLink::Ptr(parent.clone());
+                self.last_move = Movement::UP;
+            }
+        }
+    }
+
+    fn descend(&mut self) {
+        if let TreeNodeLink::Ptr(curr) = &self.curr {
+            let mut curr = curr.clone();
+
+            while let TreeNodeLink::Ptr(_) = curr.get_left() {
+                self.go_left();
+
+                if let TreeNodeLink::Ptr(updated_curr) = &self.curr {
+                    curr = updated_curr.clone();
+                }
+            }
+        }
+    }
+    fn ascend(&mut self) {
+        if let TreeNodeLink::Ptr(curr) = &self.curr {
+            let mut curr = curr.clone();
+
+            while let TreeNodeLink::Ptr(_) = curr.get_parent() {
+                self.go_up();
+
+                if let TreeNodeLink::Ptr(updated_curr) = &self.curr {
+                    curr = updated_curr.clone();
+                }
+            }
+
+            self.ascended = true;
+        }
+    }
+}
+
+impl Iterator for TreeIter {
+    type Item = TreeNodePtr;
+
+    fn next(&mut self) -> Option<TreeNodePtr> {
+        // Here I had to clone self because otherwise the borrow checker complains
+        // when I try to use go_* methods
+        match self.clone().curr {
+            TreeNodeLink::Nil => None,
+            TreeNodeLink::Ptr(curr) => {
+                match &self.last_move {
+                    Movement::LEFT => {
+                        self.go_up();
+                    }
+                    Movement::UP => {
+                        if let TreeNodeLink::Ptr(_) = curr.get_right() {
+                            self.go_right();
+                            self.descend();
+                        } else {
+                            self.curr = TreeNodeLink::Nil;
+                        }
+                    }
+                    Movement::RIGHT => {
+                        if let TreeNodeLink::Ptr(_) = curr.get_left() {
+                            self.descend();
+                        } else if let TreeNodeLink::Ptr(_) = curr.get_right() {
+                            self.go_right();
+                            self.descend();
+                        } else {
+                            if self.ascended {
+                                self.curr = TreeNodeLink::Nil;
+                            } else {
+                                self.ascend();
+                            }
+                        }
+                    }
+                    Movement::UNKNOWN => {}
+                }
+
+                Some(curr.clone())
+            }
+        }
     }
 }
 
@@ -132,4 +318,10 @@ fn main() {
     t.add_right(10).add_right(14).add_left(13);
 
     t.visit();
+
+    println!("Iterator visit");
+
+    for n in t.iter() {
+        print!("{}, ", n);
+    }
 }
